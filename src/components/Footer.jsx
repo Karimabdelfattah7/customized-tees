56382f17564f0b540a00eb0c09992156ab42bf77
Karimabdelfattah7 <karimtop20044@gmail.com>
2026-06-14 17:00:45 -0400

    Update Facebook link to customized.shirts.524

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,7 +43,7 @@ export default function Footer() {
                 </svg>
               </a>
               <a
-                href="https://www.facebook.com/customizedtees502"
+                href="https://www.facebook.com/customized.shirts.524"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Customized Tees on Facebook"
